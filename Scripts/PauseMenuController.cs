fa93bcf466d35a991edfd6865815a2a9e0d026c7
Oleksandr Panchenko <oleksandr.panchenko373@gmail.com>
2023-08-25 18:47:59 +0300

    created a new script fixing the mistakes of the previous one

---
+++ Scripts/PauseMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private bool gameIsPaused = false;
+
+    public void Button()
+    {
+        if (gameIsPaused){Resume();}
+        else{Pause();}
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
