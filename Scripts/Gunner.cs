bdf2023026c862c09f9470b0960dd858e2636a8d
Oleksandr Panchenko <oleksandr.panchenko373@gmail.com>
2023-09-05 14:57:21 +0300

    init commit

---
+++ Scripts/Gunner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class Gunner : MonoBehaviour
+{
+  [SerializeField] private int health;
+  [SerializeField] private float moveSpeed, timeBeforeDie;
+
+  private IEnumerator timeToDie;
+
+  private Animator animator;
+
+  private void Awake()
+  {
+    animator = GetComponent<Animator>();
+    timeToDie = DestroyObject(timeBeforeDie);
+  }
+
+  public void TakeDamage(int damage)
+  {
+    health -= damage;
+  }
+
+  private void CheckedIsDied(int health)
+  {
+    if (health <= 0) OnDied();
+  }
+
+  private void OnDied()
+  {
+    animator.SetBool("die", true);
+    StartCoroutine(timeToDie);
+  }
+
+  private IEnumerator DestroyObject(float waitTime)
+  {
+    yield return new WaitForSeconds(waitTime);
+    animator.SetBool("die", false);
+    Destroy(this.gameObject);
+  }
+}
